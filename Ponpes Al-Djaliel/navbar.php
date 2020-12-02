79b043bde16f1b031f9c24add409ed22ca2a1d65
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-12-02 08:58:21 +0700

    Update navbar.php

--- Ponpes Al-Djaliel/navbar.php
+++ Ponpes Al-Djaliel/navbar.php
@@ -14,11 +14,12 @@
     <nav>
         <input type="checkbox" name="" id="ribbon">
         <ul>
+            <li><a href="index.php">Halaman Utama</a></li>
             <li><a href="tentangpondok.php">Tentang Al-Djaliel</a></li>
             <li><a href="profilpengurus.php">Profil Pengurus</a></li>
             <li><a href="pembelajaransantri.php">Pembelajaran Santri</a></li>
-            <li><a href="registrasi.php">Pendaftaran Santri Baru</a></li>
-            <li><a href="index.php">Halaman Utama</a></li>
+            <li><a href="registrasi.php">Pendaftaran Santri Baru</a></li>            
+            <li><a href="datapendaftaran.php">Cek Data Pendaftaran</a></li>
         </ul>
         <label class="tie" for="ribbon"></label>
     </nav>
