bdf36f5dd34adbf6f7562ec1d3e3c88444983d5e
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-12-21 11:05:46 +0700

    update
    masalah di include news dan java script

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -87,7 +87,31 @@
 <!-- Container End-->
 
 <!-- Footer -->
-@include('layouts.FrontendFooter')
+<footer>
+        <div class="containerInfo">
+            <div class="map">
+                <div class="ImgPeta">
+                    <a href="https://www.google.com/maps/place/8%C2%B015'21.3%22S+113%C2%B038'46.4%22E/@-8.2551247,113.6473145,15z/data=!4m5!3m4!1s0x0:0x0!8m2!3d-8.2559189!4d113.6462214?hl=en" target="_blank">klik untuk lokasi Pondok Al-Djaliel</a>
+                </div>
+            </div>
+            <div class="sosmed">
+                <div class="youtube">
+                    <iframe width="" height="" src="https://www.youtube.com/embed/OmS01pKTgeY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="kontak">
+                <a href="#">
+                    <div class="ig"></div>
+                </a>
+                <a href="#">
+                    <div class="fb"></div>
+                </a>
+                <a href="#">
+                    <div class="wa"></div>
+                </a>
+            </div>
+        </div>
+    </footer>
 
 <script src="{{ asset('assets_frontend/JS/galery.js') }}"></script>
 <script src="{{ asset('assets_frontend/JS/news.js') }}"></script>
