ab841570bc0359ecef5fe33900489f160023419b
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-12-24 17:45:50 +0700

    pagination hajar bang

--- resources/views/frontend/home/news.blade.php
+++ resources/views/frontend/home/news.blade.php
@@ -9,13 +9,33 @@
         <!-- Table structure here -->
         <table class="tablepaginitation" id="our-table">
             <div id="page-news">
-                @forelse($data as $news)
-                <h1>{{ $news->judul }}</h1>
-                <img src="{{ asset('foto/'. $news->gambar) }}" alt="">
+                    <ul id="list">  
+                        <?php for ($i = 0; $i < 5; $i++){ ?>
+                        <li>
+                            @forelse($data as $news)
+                            <h1>{{ $news->judul }}</h1>
+                            <p><span>October</span> <span>01</span>,<span>2020</span>
+                            <img src="{{ asset('foto/'. $news->gambar) }}" alt="">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil ipsa quasi explicabo at, reiciendis, odio eum laudantium quaerat iste facilis iure tempore praesentium atque fugit delectus dicta impedit recusandae! Sint vero laudantium eveniet ducimus sit laborum quam voluptates aliquam similique sunt, ipsam, iusto impedit, ea modi recusandae eaque fuga reiciendis!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil ipsa quasi explicabo at, reiciendis, odio eum laudantium quaerat iste facilis iure tempore praesentium atque fugit delectus dicta impedit recusandae! Sint vero laudantium eveniet ducimus sit laborum quam voluptates aliquam similique sunt, ipsam, iusto impedit, ea modi recusandae eaque fuga reiciendis!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil ipsa quasi explicabo at, reiciendis, odio eum laudantium quaerat iste facilis iure tempore praesentium atque fugit delectus dicta impedit recusandae! Sint vero laudantium eveniet ducimus sit laborum quam voluptates aliquam similique sunt, ipsam, iusto impedit, ea modi recusandae eaque fuga reiciendis!</p>
+                            <a href="berita.php">baca selanjutnya...</a>
+                            @empty
+                            @endforelse
+                        </li>
+                        <?php } ?>
+                    </ul> 
+                    <div id="list_index" class="box"></div> 
 
-                @empty
-                <h2>belum ada</h2>
-                @endforelse
+                    <script type="text/javascript">
+                        window.addEventListener("load", function () {
+                        paginator({
+                        get_rows: function () {
+                        return document.getElementById("list").getElementsByTagName("li");
+                        },
+                        box: document.getElementById("list_index"),
+                        active_class: "color_page"
+                        });
+                        }, false);  
+                    </script>
             </div>
         </table>
         {!! $data->appends(request()->except('page'))->render() !!}
